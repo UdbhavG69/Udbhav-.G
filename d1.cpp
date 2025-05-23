c428d24395f3a0e68552775326c1b7e2a6f8f72f
UdbhavG69 <Udbhavgaonkar@gmail.com>
2025-05-23 15:09:52 +0530

    Add files via upload

---
+++ d1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int add (int a, int b)
+{ 
+    int c;
+    c=a+b;
+    return c;
+}
+
+int main ()
+{
+    int reasult=add(10,20);
+    cout<<reasult<<endl;
+    }
---
+++ file4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Area{
+public:
+    int length = 0, breath = 0, radius = 0;
+    Area(int in_length, int in_breath) {
+        length = in_length;
+        breath = in_breath;
+    }
+    Area(int in_radius) {
+        radius = in_radius;
+    
+    }
+    void result_area(){
+        if (radius == 0){
+            cout << length * breath << endl;
+        } else{
+
+            cout << breath* breath*3.14 << endl;
+        }
+    }    
+ };
+
+int main(){
+    Area a1(10);
+    a1.result_area();
+}
